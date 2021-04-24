c35e2a825abcac4591cd437d60ddf20e9d9ed555
Nur Muhammad 'Ainul Yaqin <64957624+masnurrm@users.noreply.github.com>
2021-04-24 12:01:30 +0700

    Update image-blending-pyramid-stackoverflow.py

--- image-pyramids/image-blending-pyramid-stackoverflow.py
+++ image-pyramids/image-blending-pyramid-stackoverflow.py
@@ -19,49 +19,52 @@ for i in range(6):
     G = cv2.pyrDown(G)
     gpB.append(G)
 
+# generate Laplacian Pyramid for A
 lpA = [gpA[5]]
-for i in range(6,0,-1):
-    print(i)
+for i in range(6, 0, -1):
+    # print(i)
     GE = cv2.pyrUp(gpA[i])
-    GE=cv2.resize(GE,gpA[i - 1].shape[-2::-1])
-    L = cv2.subtract(gpA[i-1],GE)
+    GE=cv2.resize(GE, gpA[i-1].shape[-2::-1])
+    L = cv2.subtract(gpA[i-1], GE)
+    # print(L.shape)
     lpA.append(L)
 
 # generate Laplacian Pyramid for B
-
 lpB = [gpB[5]]
-for i in range(6,0,-1):
-    print(i)
+for i in range(6, 0, -1):
+    # print(i)
     GE = cv2.pyrUp(gpB[i])
-    GE = cv2.resize(GE, gpB[i - 1].shape[-2::-1])
-    L = cv2.subtract(gpB[i-1],GE)
-    print(L.shape)
+    GE = cv2.resize(GE, gpB[i-1].shape[-2::-1])
+    L = cv2.subtract(gpB[i-1], GE)
+    # print(L.shape)
     lpB.append(L)
 
 # Now add left and right halves of images in each level
 LS = []
 lpAc=[]
 for i in range(len(lpA)):
-    b=cv2.resize(lpA[i],lpB[i].shape[-2::-1])
+    b=cv2.resize(lpA[i], lpB[i].shape[-2::-1])
     lpAc.append(b)
-print(len(lpAc))
-print(len(lpB))
+    
+# print(len(lpAc))
+# print(len(lpB))
+
 j=0
 for i in zip(lpAc,lpB):
     la,lb = i
-    rows,cols,dpt = la.shape
-    ls = np.hstack((la[:,0:cols//2], lb[:,cols//2:]))
-    j=j+1
+    rows, cols, dpt = la.shape
+    ls = np.hstack((la[:, 0:cols//2], lb[:,cols//2:]))
+    j = j + 1
     LS.append(ls)
 
 ls_ = LS[0]
-for i in range(1,6):
+for i in range(1, 6):
     ls_ = cv2.pyrUp(ls_)
     ls_= cv2.resize(ls_, LS[i].shape[-2::-1])
     ls_ = cv2.add(ls_, LS[i])
 
 # image with direct connecting each half
 B= cv2.resize(B, A.shape[-2::-1])
-real = np.hstack((A[:,:cols//2],B[:,cols//2:]))
-cv2.imwrite('Pyramid_blending2.jpg',ls_)
-cv2.imwrite('Direct_blending.jpg',real)
+real = np.hstack((A[:,:cols//2], B[:,cols//2:]))
+cv2.imwrite('minasana1.jpg', ls_)
+cv2.imwrite('minasana2.jpg', real)
