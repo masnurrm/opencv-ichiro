08deac87472b4ecba74ba98518d577a9dc228ac4
Nur Muhammad 'Ainul Yaqin <64957624+masnurrm@users.noreply.github.com>
2021-04-24 09:09:07 +0700

    Create affine-transformation.py

---
+++ geometric-transformations-img/affine-transformation.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv.imread('wendy.jpg')
+
+rows, cols, ch = img.shape
+pts1 = np.float32([[50,50], [200,50], [50,200]])
+pts2 = np.float32([[10,100], [200,50], [100,250]])
+M = cv.getAffineTransform(pts1, pts2)
+dst = cv.warpAffine(img,M,(cols, rows))
+
+plt.subplot(121), plt.imshow(img), plt.title('Input')
+plt.subplot(122), plt.imshow(dst), plt.title('Output')
+plt.show()
