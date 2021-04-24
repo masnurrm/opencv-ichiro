3783786e69840d852c62ac03c4ed02e1517f34f9
Nur Muhammad 'Ainul Yaqin <64957624+masnurrm@users.noreply.github.com>
2021-04-24 09:11:17 +0700

    Create average-blur.py

---
+++ smoothing-image/average-blur.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv.imread('mina.jpg')
+blur = cv.blur(img,(5,5))
+
+plt.subplot(121), plt.imshow(img), plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122), plt.imshow(blur), plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+
+plt.show()
